7300b2373f9251feddd0d1c9da2c18e64d4e0599
namtrinh <sabo_nt>
2021-08-04 11:30:08 +0700

    Refs[TASK[39336] Create Launch Screen

--- Crypto-Currency/Crypto-Currency/Scenes/FavoriteViewController.swift
+++ Crypto-Currency/Crypto-Currency/Scenes/FavoriteViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FavoriteViewController: UIViewController {
+final class FavoriteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
