53eab556c55aa34c4c15fcce506456d57001be72
namtrinh <sabo_nt>
2021-08-05 16:01:08 +0700

    Refs[TASK][39340] Handle Event Get Started Screen

--- Crypto-Currency/Crypto-Currency/Scenes/HomeViewController.swift
+++ Crypto-Currency/Crypto-Currency/Scenes/HomeViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        showGetStartedScreen()
     }
-
+    
+    private func showGetStartedScreen() {
+        if !UserDefaults.standard.bool(forKey: "didLaunchBefore") {
+            UserDefaults.standard.set(true, forKey: "didLaunchBefore")
+            guard let getstartedScreen = storyboard?.instantiateViewController(identifier: "GetStartedViewController") as? GetStartedViewController else {
+                return
+            }
+            getstartedScreen.modalPresentationStyle = .fullScreen
+            present(getstartedScreen, animated: true, completion: nil)
+        }
+    }
+    
 }
